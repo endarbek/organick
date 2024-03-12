ab85cc33edc55f985d4803956b4e57287d791d16
malik <jumakadyrovmalik>
2024-03-12 12:07:22 +0600

    shop

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
-
+import Styles from "./styles.module.css";
 const page = () => {
-  return <div>SHOP</div>;
+  return (
+    <div>
+      <h1 className={Styles.con}>SHOP</h1>
+    </div>
+  );
 };
 
 export default page;
