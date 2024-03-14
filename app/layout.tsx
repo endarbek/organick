3cd8b47718b6cf7582ca2cd6ff8e1bd55c0d1a0f
Эндарбек <endarashiraliev@gmail.com>
2024-03-14 10:49:17 +0600

    modification

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,17 @@ import Newsletter from "@/components/Newsletter/Newsletter";
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700", "900"],
   subsets: ["latin"],
+  variable: "--font-roboto",
 });
 const openSans = Open_Sans({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
+  variable: "--font-openSans",
 });
 const yellowTail = Yellowtail({
   weight: ["400"],
   subsets: ["latin"],
+  variable: "--font-yellowtail",
 });
 
 export const metadata: Metadata = {
@@ -30,7 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${openSans} ${roboto} ${yellowTail}`}>
+      <body
+        className={`${openSans.variable} ${roboto.variable} ${yellowTail.variable}`}
+      >
         <div className="container">
           <Navbar />
           {children}
